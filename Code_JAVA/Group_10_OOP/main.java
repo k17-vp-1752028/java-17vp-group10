6e5afb2ea88b45666e10ce912085ac679aec27fd
1752022-VP <1752022@student.hcmus.edu.vn>
2020-05-20 21:56:18 +0700

    Code_Java update 2 20/5/2020
    Code_Java update 2 20/5/2020

---
+++ Code_JAVA/Group_10_OOP/main.java
@@ -0,0 +1,18 @@
+package Group_10_OOP;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   
+		        Group_10_OOP.PatientF0 f0 = new Group_10_OOP.PatientF0();
+		        f0.Input();
+		        f0.Output();
+
+		        System.out.println("====================");
+
+//		        Group_10_OOP.PatientFN fN = new Group_10_OOP.PatientFN();
+//		        fN.Input();
+//		        fN.Output();
+		    }
+}
