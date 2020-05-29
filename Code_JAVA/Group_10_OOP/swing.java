4af4ac992915a9e521987300e4d48024929e893a
1752022-VP <1752022@student.hcmus.edu.vn>
2020-05-29 20:55:23 +0700

    Update swing
    Update Login_Register 29/5

---
+++ Code_JAVA/Group_10_OOP/swing.java
@@ -0,0 +1,7 @@
+package Group_10_OOP;
+
+public class swing {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- Practise/src/HomeSweetHome/Login_register.java
+++ Practise/src/HomeSweetHome/Login_register.java
@@ -1,6 +1,7 @@
 package HomeSweetHome;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,10 +17,17 @@ public class Login_register extends JFrame{
         add(rootPanel);
         setTitle("Login Register");
         setSize(500,200);
+        setLayout(new FlowLayout(FlowLayout.CENTER, 10000, 10));
+        JHyperlink linkLabel = new JHyperlink("Create new account");
+        linkLabel.setURL("https://www.youtube.com/watch?v=G1Zo3UKzB4A");
+        getContentPane().add(linkLabel);
+
+
+
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            JOptionPane.showMessageDialog(rootPanel, "Ahihi");
+                JOptionPane.showMessageDialog(rootPanel, "Ahihi");
             }
         });
         cancleButton.addActionListener(new ActionListener() {
@@ -28,5 +36,7 @@ public class Login_register extends JFrame{
                 JOptionPane.showMessageDialog(rootPanel, "Ohoho");
             }
         });
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
